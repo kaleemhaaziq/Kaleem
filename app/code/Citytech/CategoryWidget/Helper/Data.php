0df3567dd065980a32973296a7f7d379226c4bfb
Kaleem Ahmad <kaleem@citytechcorp.com>
2026-07-29 13:48:22 +0530

    commit Helper Data

---
+++ app/code/Citytech/CategoryWidget/Helper/Data.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Citytech\CategoryWidget\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+    /**
+     * XML Config Path
+     */
+    public const XML_PATH_ENABLED = 'category_widget/general/enabled';
+
+    /**
+     * Check if module is enabled
+     *
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_ENABLED,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Get Config Value
+     *
+     * @param string $path
+     * @param int|null $storeId
+     * @return mixed
+     */
+    public function getConfigValue(string $path, ?int $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $path,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+}
